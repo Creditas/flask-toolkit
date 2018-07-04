6638c79ab546ca72a896cb7cc957546929a0d3da
Aurelio Saraiva <aureliosaraiva@gmail.com>
2018-07-04 13:32:18 -0300

    created package for flask-toolkit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # flask-toolkit
+
+
+```bash
+git clone git@github.com:Creditas/flask-toolkit.git
+cd flask-toolkit
+source env/bin/activate
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flask_toolkit",
+    version="0.0.1",
+    author="Aurelio Saraiva",
+    author_email="aurelio.saraiva@creditas.com.br",
+    description="Flask toolkit for Domain Driven Design (DDD)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/creditas/flask-toolkit",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
