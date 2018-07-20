d467230f2f110552e57ecf0b02e5deecb59af109
Aurelio Saraiva <aureliosaraiva@gmail.com>
2018-07-20 18:46:45 -0300

    added wrappper default on representer

--- flask_toolkit/shared/representer.py
+++ flask_toolkit/shared/representer.py
@@ -4,7 +4,7 @@ from marshmallow import Schema
 class Representer(Schema):
 
     def render(self, data, wrapper=None):
-        wrapper = wrapper or getattr(self.Meta, 'wrapper', None)
+        wrapper = wrapper or getattr(self.Meta, 'wrapper', 'data')
 
         if wrapper is None:
             raise NotImplementedError('Wrapper not Implementend')
