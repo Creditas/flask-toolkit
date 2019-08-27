3b928a7f516967047f458ba73aa0585603d9c296
Adinan Paiva <paiva.adinan@gmail.com>
2019-08-27 08:26:52 -0300

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flask_toolkit",
-    version="0.0.19",
+    version="0.0.20",
     author="Aurelio Saraiva",
     author_email="aurelio.saraiva@creditas.com.br",
     description="Flask toolkit for Domain Driven Design (DDD)",
