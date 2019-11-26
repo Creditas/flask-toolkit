a714a0e32bab2cd22124d9fc2dcbf9b5b9e39bfd
Lucas Queiroz <lucas.queiroz@creditas.com.br>
2019-11-26 16:30:36 -0300

    Rename method

--- flask_toolkit/shared/repository.py
+++ flask_toolkit/shared/repository.py
@@ -51,7 +51,7 @@ class Repository(object):
 
         return self.session.query(entity)
 
-    def filter_with_pagination(self, limit, offset, filters={}):
+    def fetch_paginated(self, limit, offset, filters={}):
         return self.query().limit(limit).offset(offset).filter_by(filters).all()
 
     def __dispatch_domain_events(self, entity):
